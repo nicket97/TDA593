88ac7a285e4e205a632c1df75baa8eec14810e3f
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2018-12-06 14:02:00 +0100

    Implement Mission roughly

--- ROVU/Core/src/model/Mission.java
+++ ROVU/Core/src/model/Mission.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mission {
+    private List<Point> mission;
+    private List<Strategy> strategies;
+
+    public Mission(List<Point> missionPoints) {
+        mission = missionPoints;
+    }
+
+    @Override
+    public String toString() {
+        List<String> points = new ArrayList<>();
+        mission.forEach(point -> {
+            double x = point.getX();
+            double y = point.getY();
+            String pointString = "(" + x + "," + y + ")";
+            points.add(pointString);
+        });
+        return points.toString();
+    }
 
 }
