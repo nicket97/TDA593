6aab6b92dbccec1534e3332561c37fd9602291da
nicket97 <niclas.o.johansson@telia.com>
2018-12-06 15:41:44 +0100

    adding xSize and ySize

--- ROVU/Core/src/model/Environment.java
+++ ROVU/Core/src/model/Environment.java
@@ -5,9 +5,15 @@ import java.util.Map;
 
 public class Environment implements IEnvironment {
     private Node[][] map;
+    int xSize;
+    int ySize;
     
 	public Environment (Node[][] map){
+
+		xSize = map.length;
+		ySize = map[0].length;
 		this.map=map;
+
 	}
 	
 	public Node getEnvironment (Point position){
