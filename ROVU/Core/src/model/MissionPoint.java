67079748a1ddf3de03819b200d5628bd366d3f0e
nicket97 <niclas.o.johansson@telia.com>
2018-12-10 20:12:18 +0100

    adding priority

--- ROVU/Core/src/model/MissionPoint.java
+++ ROVU/Core/src/model/MissionPoint.java
@@ -8,11 +8,23 @@ import java.util.List;
 public class MissionPoint {
     Point point;
     List<Constraint> constraints = new ArrayList<Constraint>();
+    int priority;
     public MissionPoint(Point p){
         point = p;
+        priority = 0;
     }
     public MissionPoint(Point p, List<Constraint> con){
         point = p;
         constraints = con;
+        priority = 0;
     }
+    public MissionPoint(Point p, int prio){
+        point = p;
+        priority = prio;
+    } public MissionPoint(Point p, List<Constraint> con, int prio){
+        point = p;
+        constraints = con;
+        priority = prio;
+    }
+
 }
