a83c6feee6e0ac10039b10fc4d11e28a4c70aea3
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2018-12-13 00:21:43 +0100

    Implement dummy reward calculator

--- ROVU/Core/src/model/RewardCalculator.java
+++ ROVU/Core/src/model/RewardCalculator.java
@@ -1,19 +1,97 @@
 package model;
 
+import controller.DataHandler;
+import controller.RobotController;
+import project.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class RewardCalculator {
-    static boolean isA = true;
-    public static void calculateReward() {
-        if(isA){
-            calculateProcedureA();
+    // TODO: Retrieve singleton RobotController as the DataHandler
+    // DataHandler dataHandler = new RobotController();
+    private boolean isA = true;
+
+    // Return a list of random nodes
+    private List<Node> mockNodes() {
+        Random r = new Random();
+        int rangeMax = 10;
+        int rangeMin = -10;
+        List<Node> nodes = new ArrayList<>();
+        double r1 = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        double r2 = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        for (int i=0; i<4; i++) {
+            nodes.add(new Node(r.nextBoolean(), r.nextBoolean(), r.nextBoolean(), i, new Point(r1, r2)));
         }
-        else{
-            calculateProcedureB();
-        }
+        return nodes;
     }
 
-    private static void calculateProcedureA() {
+    public int calculateReward() {
+        int rewardPoints = -99; // If it returns -99, then something's gone wrong
+        // TODO: Get the nodes from the RobotController
+        List<Node> nodes = mockNodes();
+        boolean inPhysicalArea = inPhysicalArea(nodes);
+        boolean inLogicalArea = inLogicalArea(nodes);
+        // TODO: Implement PhysicalAreas, else it won't calculate anything
+        if (isA){
+            rewardPoints = calculateProcedureA(nodes);
+            if (inLogicalArea) isA = !isA;
+        }
+        else {
+            rewardPoints = calculateProcedureB(nodes);
+            if (inPhysicalArea) isA = !isA;
+        }
+        return rewardPoints;
     }
 
-    private static void calculateProcedureB() {
+    private int calculateProcedureA(List<Node> nodes) {
+        int points = 0;
+        for (Node node : nodes) {
+            /* TODO: Need to update Node so that it
+             * differentiates between CONSULTING_ROOM
+             * and SURGERY_ROOM */
+            if (node.isRoom()) {
+                points++;
+            }
+//            if (node.isConsultingRoom()) { or node.isRoom(Hospital.CONSULTING_ROOM)
+//                points += 10;
+//            } else if (node.isSurgeryRoom()) { or node.is(Hospital.SURGERY_ROOM)
+//                points += 20;
+//            }
+        }
+        return points;
+    }
+
+    private int calculateProcedureB(List<Node> nodes) {
+        int points = 0;
+        for (Node node : nodes) {
+            if (node.isWifi()) {
+                points += 10;
+            } else if (node.isEating()) {
+                points += 20;
+            }
+        }
+        return points;
+    }
+
+    private boolean inPhysicalArea(List<Node> nodes) {
+        boolean inPhysicalArea = false;
+        for (Node node : nodes) {
+            if (!node.isWall()) {
+                inPhysicalArea = true;
+            }
+        }
+        return inPhysicalArea;
+    }
+
+    private boolean inLogicalArea(List<Node> nodes) {
+        boolean inLogicalArea = false;
+        for (Node node : nodes) {
+            if (node.isWifi() || node.isEating()) {
+                inLogicalArea = true;
+            }
+        }
+        return inLogicalArea;
     }
 }
