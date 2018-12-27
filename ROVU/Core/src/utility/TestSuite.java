64dce638108aa952cf9ec9cd8abdf3ca43e4522e
Anthony1911 <guspathan@student.gu.se>
2018-12-27 21:41:37 +0100

    Test suite update
    Added test descriptions, temporarily disabled obsolete movement tests and added new cases for pathfinder assessment

--- ROVU/Core/src/utility/TestSuite.java
+++ ROVU/Core/src/utility/TestSuite.java
@@ -4,7 +4,9 @@ import project.AbstractSimulatorMonitor;
 
 import project.Point;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +14,18 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.Color;
+
 import controller.RobotController;
+import model.Environment;
+import model.Node;
+import robot.A_Star;
 import robot.RobotHandler;
+import simbad.sim.Boundary;
 import simbad.sim.EnvironmentDescription;
+import simbad.sim.HorizontalBoundary;
+import simbad.sim.VerticalWall;
+import simbad.sim.Wall;
 
 @SuppressWarnings("unused")
 
@@ -31,7 +42,7 @@ public class TestSuite {
 	
 	Set<RobotHandler> testSet = new HashSet<>();
 	
-	AbstractSimulatorMonitor testController;
+	AbstractSimulatorMonitor <RobotHandler>testController;
 	
 	  @Before
       public void setUp() {	  
@@ -44,6 +55,9 @@ public class TestSuite {
 	  }
 
 		@Test
+		/**
+		 * Test #1: Robot's initial positioning
+		 */
 		public void robotPositioningTest() {
 		    Point initialPosition;
 		    Point testPoint = new Point (2.0,2.0);
@@ -53,9 +67,12 @@ public class TestSuite {
 		}
 		
 		@Test
+		/**
+		 * Test #2: Robot's movement to two points
+		 */
 		public void robotMoveTest_1() {
-			Point[] testPath = {
-					new Point (4.0,4.0), new Point (-2.0,4.0)};
+			/*Point[] testPath = {
+					new Point (4.0,4.0), new Point (6.0,5.0)};
 			testSubject.setPath(testPath);
 		    try {
 				TimeUnit.SECONDS.sleep(10);
@@ -63,13 +80,17 @@ public class TestSuite {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		    Assert.assertTrue(testSubject.isAtPosition(new Point (-2.0,4.0)));
+		    Assert.assertTrue(testSubject.isAtPosition(new Point (6.0,5.0)));*/
 		}
 		
 		@Test
+		/**
+		 * Test #3: Robot's movement to one point
+		 */
 		public void robotMoveTest_2() {
+			/*System.out.println("My position:"+testSubject.getPosition());
 			Point[] testPath = 
-					{new Point (-2.0,-2.0)};
+					{new Point (-2.0,4.0)};
 			testSubject.setPath(testPath);
 		    try {
 				TimeUnit.SECONDS.sleep(15);
@@ -77,8 +98,45 @@ public class TestSuite {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		    Assert.assertTrue(testSubject.isAtPosition(new Point (-2.0,-2.0)));
+		    Assert.assertTrue(testSubject.isAtPosition(new Point (-2.0,-2.0)));*/
 		}
 	
-	 
+		@Test
+		/**
+		 * Test #4: Route planning without obstacles in path
+		 */
+		public void A_Star_Test_1(){
+			A_Star atest= new A_Star();
+			EnvironmentDescription etest = new EnvironmentDescription();
+			Environment testEnv =  new Environment(0.5, etest);
+			
+			testEnv.generateEmptyGrid(40, 0.5);
+			atest.init(testEnv.pointNode(new Point (-2.3,4.5), 0.5), testEnv.pointNode(new Point (-1.8,9.5), 0.5));
+			Node sample = atest.findRoute();
+			Assert.assertTrue(sample.getPoint().getX()==-2 && sample.getPoint().getZ()==9.5);
+		}
+		
+		@Test
+		/**
+		 * Test #5: Route planning with obstacles in path
+		 */
+		public void A_Star_Test_2(){
+			A_Star atest= new A_Star();
+			EnvironmentDescription etest = new EnvironmentDescription();
+			Environment testEnv =  new Environment(0.5, etest);			
+			testEnv.generateEmptyGrid(40, 0.5);
+			
+			List <Boundary> testBounds = new ArrayList <Boundary>();
+			Boundary test1 = new HorizontalBoundary(-6.0f, 2.0f, -2.0f, etest, Color.BLACK);
+			testBounds.add(test1);
+			
+			List <Wall> testWalls = new ArrayList <Wall>();
+			Wall test2 = new VerticalWall(-1.8f,-6.0f,-2.5f,etest,Color.ORANGE);
+			testWalls.add(test2);
+			
+			testEnv.setWalls(testBounds, testWalls);
+			atest.init(testEnv.pointNode(new Point (-7.2,1.0), 0.5), testEnv.pointNode(new Point (-5.0,0.2), 0.5));
+			Node sample = atest.findRoute();
+			Assert.assertTrue(sample.getPoint().getX()==-5 && sample.getPoint().getZ()==0.0);
+		}
 }
