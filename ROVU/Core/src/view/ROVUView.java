81245e2d416ca4d4b2a8bb649f039b8068c18756
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2019-01-03 23:27:28 +0100

    Launch MissionEditor with ROVUView

--- ROVU/Core/src/view/ROVUView.java
+++ ROVU/Core/src/view/ROVUView.java
@@ -6,7 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ROVUView extends Application {
+    Map<String, String> parameters = new HashMap<>();
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -14,9 +18,27 @@ public class ROVUView extends Application {
 		Parent view = FXMLLoader.load(getClass().getResource("../fxml/ROVUView.fxml"));
 		Scene scene = new Scene(view);
 		primaryStage.setScene(scene);
+		primaryStage.setX(800);
+		primaryStage.setY(550);
 		primaryStage.show();
-
 		primaryStage.setOnCloseRequest(event -> System.exit(0));
+
+		parameters = getParameters().getNamed();
+
+		MissionEditorView missionEditorView = new MissionEditorView();
+		if (parameters.size() != 0 && parameters.containsKey("customMission")) {
+		    if (parameters.get("customMission").equals("TRUE")) {
+                missionEditorView.setCustomMission(true);
+            }
+        } else {
+            missionEditorView.setCustomMission(false);
+        }
+		Stage missionEditorStage = missionEditorView.getStage();
+		missionEditorStage.setTitle("Mission Editor");
+		missionEditorStage.setX(800);
+		missionEditorStage.setY(50);
+		missionEditorStage.show();
+		missionEditorStage.setOnCloseRequest(event -> System.exit(0));
 	}
 
 	public static void main(String[] args) {
