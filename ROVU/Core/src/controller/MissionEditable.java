257b091ed304dd99da2e69dbc3ef968c8eee6a00
Tuyen Ngo <tuyen.ngo@hyperlab.se>
2019-01-10 15:34:34 +0100

    Change interface

--- ROVU/Core/src/controller/MissionEditable.java
+++ ROVU/Core/src/controller/MissionEditable.java
@@ -12,11 +12,4 @@ public interface MissionEditable {
 	 */
 void createMission(ActionEvent event);
 
-
-    /** 
-     * Edits the current mission
-     * @param event The event that is sent through the JavaFX Button.
-     * @return
-     */
-void editMission(ActionEvent event);
 }
